9cf81a591cf6cb58064611446942820315960b89
2021191247 <2021191247@email.szu.edu.cn>
2024-06-11 21:14:16 +0800

    实验发布补充

--- pages/manager/index.vue
+++ pages/manager/index.vue
@@ -4,28 +4,28 @@
       <el-tab-pane label="用户管理">
         <users />
       </el-tab-pane>
-      <el-tab-pane label="题目管理">
-        <!-- TODO -->
-        TODO
+      <el-tab-pane label="实验发布">
+        <exp-publish />
       </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script setup lang="ts">
-  import Users from './components/users.vue'
+import Users from './components/users.vue';
+import ExpPublish from './components/exp-publish.vue';
 
-  useHead({
-    title: '系统管理',
-  })
+useHead({
+  title: '系统管理',
+});
 
-  const router = useRouter()
-  const userStore = useUserStore()
-  const { loggedIn, isTeacher } = storeToRefs(userStore)
+const router = useRouter();
+const userStore = useUserStore();
+const { loggedIn, isTeacher } = storeToRefs(userStore);
 
-  watchEffect(() => {
-    if (loggedIn.value && !isTeacher.value) {
-      router.replace('/')
-    }
-  })
+watchEffect(() => {
+  if (loggedIn.value && !isTeacher.value) {
+    router.replace('/');
+  }
+});
 </script>
